1af2fcd415db426dc93b73e09f8e2e6975bb9e46
Admin <admin@VladMacBookPro.local>
2022-12-31 00:23:45 +0200

    add drive inquirer

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('It work!')
+// const userName = prompt('Enter your name:');
+const userAge = +prompt('How old are you ?');
+const drive = confirm('Are you drive a car ?');
+
+if(userAge > 18) {
+    console.log(userAge);
+} else if(drive < true) {
+    console.log(drive);
+} else {
+    alert('You drive with parents?');
+} 
+
+if(userAge > 18) {
+    console.log(userAge);
+} else if(drive > false) {
+    console.log(drive); 
+} else {
+    alert('You are a law abiding citizen!');
+} 
+
+if(userAge < 18) {
+    console.log(userAge);
+} else if(drive > false) {
+    console.log(drive);
+} else {
+    alert('You like walking ?)');
+} 
+
+if(userAge < 18) {
+    console.log(userAge);
+} else if(drive < true) {
+    console.log(drive);
+} else {
+    alert('Have a good drive!');
+} 
