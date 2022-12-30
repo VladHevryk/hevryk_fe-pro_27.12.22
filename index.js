0724ea32c81d7414291b038c0d34e852cd79b783
Admin <admin@VladMacBookPro.local>
2022-12-30 23:03:51 +0200

    add greeting message

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('It work!')
+const userName = prompt('Enter your name:');
+const userAge = prompt('Enter your age:');
+const userCity = prompt('Enter your city:');
+
+const message = `Hello dear ${userName}, ${userAge} is such a good years old, ${userCity} - it is my lovely place, nice to meet you!`;
+
+console.log(message);
