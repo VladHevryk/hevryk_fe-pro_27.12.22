b7da7e13b8352e9767830dbc3c1b59952dd8295e
Admin <admin@VladMacBookPro.local>
2023-01-14 15:54:28 +0200

    add some changes

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-// // first part of home work 7
+// first part of home work 7
 
-// for (let halfOfNum = 20; halfOfNum <= 30; halfOfNum += 0.5) {
-//     console.log(halfOfNum);
-//     continue;
-// };
+for (let halfOfNum = 20; halfOfNum <= 30; halfOfNum += 0.5) {
+    console.log(halfOfNum);
+    continue;
+};
 
-// // second part of home work 7
+// second part of home work 7
 
-// for (let priceOfDollar = 10; priceOfDollar <= 100; priceOfDollar++) {
-//     if (priceOfDollar % 10 == 0) {
-//         console.log(priceOfDollar * 27);
-//     }
-// };
+for (let priceOfDollar = 10; priceOfDollar <= 100; priceOfDollar++) {
+    if (priceOfDollar % 10 == 0) {
+        console.log(priceOfDollar * 27);
+    }
+};
 
-// // third part of home work 7 
+// third part of home work 7 
 
-// let primeNumb = 79;
+let primeNumb = 79;
 
-// for (let i = 0; i <= 100 ; i++) {
-//   let multiplyNum = i * i;
-//   if (multiplyNum <= primeNumb) {
-//     console.log(multiplyNum);
-//     continue;
-//   }
-// };
+for (let i = 0; i <= 100 ; i++) {
+  let multiplyNum = i * i;
+  if (multiplyNum <= primeNumb) {
+    console.log(multiplyNum);
+    continue;
+  }
+};
     
-// // forth part of home work 7
+// forth part of home work 7
 
-// nextPrime: 
-// for (let primeNum = 2; primeNum < 20; primeNum++) {
+nextPrime: 
+for (let primeNum = 2; primeNum < 20; primeNum++) {
 
-//   for (let justNum = 2; justNum < primeNum; justNum++) {
-//     if (primeNum % justNum == 0) 
-//     continue nextPrime;
-//   }
-//   console.log(primeNum);
-// }; 
+  for (let justNum = 2; justNum < primeNum; justNum++) {
+    if (primeNum % justNum == 0) 
+    continue nextPrime;
+  }
+  console.log(primeNum);
+}; 
 
 // fifth part of home work 7 
 
