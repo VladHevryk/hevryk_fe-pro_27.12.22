44cde4bb25c09d0b04c259ee504510b3e194a41a
Admin <admin@VladMacBookPro.local>
2023-03-13 23:00:18 +0200

    add home work 16

---
+++ index.js
@@ -0,0 +1,329 @@
+const product = {
+    shoes: [
+        {
+            href: '/img/shoes1.jpg',
+            name: 'shoes first',
+            price: '3999 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/shoes2.jpg',
+            name: 'shoes second',
+            price: '1499 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/shoes3.jpg',
+            name: 'shoes third',
+            price: '2399 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/shoes4.jpg',
+            name: 'shoes forth',
+            price: '4599 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/shoes5.jpg',
+            name: 'shoes fivth',
+            price: '1099 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        }
+    ],
+    hoody: [
+        {
+            href: '/img/hoody1.jpg',
+            name: 'hoody first',
+            price: '3999 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hoody2.jpg',
+            name: 'hoody second',
+            price: '1499 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hoody3.jpg',
+            name: 'hoody third',
+            price: '2399 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hoody4.jpg',
+            name: 'hoody forth',
+            price: '4599 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hoody5.jpg',
+            name: 'hoody fivth',
+            price: '1099 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        }
+    ],
+    tshirt: [
+        {
+            href: '/img/tshirt1.jpg',
+            name: 't-shirt first',
+            price: '999 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/tshirt2.jpg',
+            name: 't-shirt second',
+            price: '499 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/tshirt3.jpg',
+            name: 't-shirt third',
+            price: '399 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/tshirt4.jpg',
+            name: 't-shirt forth',
+            price: '599 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/tshirt5.jpg',
+            name: 't-shirt fivth',
+            price: '1099 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        }
+    ],
+    jeans: [
+        {
+            href: '/img/jeans1.jpg',
+            name: 'jeans first',
+            price: '999 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/jeans2.jpg',
+            name: 'jeans second',
+            price: '499 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/jeans3.jpg',
+            name: 'jeans third',
+            price: '399 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/jeans4.jpg',
+            name: 'jeans forth',
+            price: '599 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/jeans5.jpg',
+            name: 'jeans fivth',
+            price: '1099 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        }
+    ],
+    bags: [
+        {
+            href: '/img/bag1.jpg',
+            name: 'bag first',
+            price: '3999 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/bag2.jpg',
+            name: 'bag second',
+            price: '2499 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/bag3.jpg',
+            name: 'bag third',
+            price: '6399 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/bag4.jpg',
+            name: 'bag forth',
+            price: '4599 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/bag5.jpg',
+            name: 'bag fivth',
+            price: '9099 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        }
+    ],
+    hats: [
+        {
+            href: '/img/hat1.jpg',
+            name: 'hat first',
+            price: '999 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hat2.jpg',
+            name: 'hat second',
+            price: '299 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hat3.jpg',
+            name: 'hat third',
+            price: '699 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hat4.jpg',
+            name: 'hat forth',
+            price: '1599 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        },
+        {
+            href: '/img/hat5.jpg',
+            name: 'hat fivth',
+            price: '3099 UAH',
+            descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam in sed perferendis, enim possimus'
+        }
+    ]
+};
+
+const catalog = document.querySelector('.catalog');
+const mainList = document.querySelector('.main__list');
+const wrapperBlock = document.querySelector('.product__wrapp');
+const popup = document.querySelector('.popup');
+const popupContent = document.querySelector('.popup__content');
+const popupCloseIcon = document.querySelector('.popup__close');
+
+const actionTypes = {
+    shoes: () => productCatalog('shoes'),
+    hoody: () => productCatalog('hoody'),
+    tshirt: () => productCatalog('tshirt'),
+    jeans: () => productCatalog('jeans'),
+    bag: () => productCatalog('bags'),
+    hat: () => productCatalog('hats')
+};
+
+mainList.addEventListener('click', (e) => {
+    const targetAction = e.target.dataset.action;
+    actionTypes[targetAction]();
+});
+
+function productCatalog(produktKey) {
+    if(!wrapperBlock) return;
+    wrapperBlock.innerHTML = '';
+    Object.values(product[produktKey]).forEach((el) => {
+        const mainBlock = document.createElement('div');
+        mainBlock.classList.add('product__block');
+        wrapperBlock.appendChild(mainBlock);
+        const img = document.createElement('img');
+        img.src = el.href;
+        img.classList.add('product__img');
+        mainBlock.appendChild(img);
+        const title = document.createElement('h1');
+        title.innerText = el.name;
+        title.classList.add('product__title');
+        mainBlock.appendChild(title);
+        const infoBlock = document.createElement('div');
+        infoBlock.classList.add('product__info');
+        mainBlock.appendChild(infoBlock);
+        const price = document.createElement('span');
+        price.innerText = el.price;
+        price.classList.add('product__price');
+        infoBlock.appendChild(price);
+        const descr = document.createElement('p');
+        descr.innerText = el.descr;
+        descr.classList.add('product__descr');
+        infoBlock.appendChild(descr);
+        const btn = document.createElement('button');
+        btn.innerText = 'buy now!';
+        btn.classList.add('product__btn');
+        infoBlock.appendChild(btn);
+        mainBlock.addEventListener('click', (e) => {
+            const blockClass = document.querySelectorAll('.product__info');
+            blockClass.forEach(el => el.classList.remove('product__info-active'));
+            infoBlock.classList.add('product__info-active');
+            btn.addEventListener('click', (e) => {
+                popup.classList.add('popup-active');
+                popupContent.classList.add('popup__content-active');
+                popupCloseIcon.addEventListener('click', (e) => {
+                    popup.classList.remove('popup-active');
+                    popupContent.classList.remove('popup__content-active');
+                    wrapperBlock.innerHTML = '';
+                });
+            });
+        });
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// img.addEventListener('click', (e) => {
+//     // popup.classList.add('popup-active');
+//     console.log('hello');
+// });
+
+
+
+
+// function productCatalog(produktKey) {
+//     const wrapperBlock = document.createElement('div');
+//     wrapperBlock.classList.add('product__wrapp');
+//     catalog.appendChild(wrapperBlock);
+//     mainList.addEventListener('click', (e) => {
+//         if(!wrapperBlock) return;
+//         wrapperBlock.innerHTML = '';
+//         wrapperBlock.classList.add('product__wrapp-active');
+//     });
+
+
+
+// function productCatalog(produktKey) {
+//     const wrapperBlock = document.createElement('div');
+//     wrapperBlock.classList.add('product__wrapp');
+//     catalog.appendChild(wrapperBlock);
+//     mainList.addEventListener('click', (e) => {
+//         if(!wrapperBlock) return;
+//         wrapperBlock.innerHTML = '';
+//         wrapperBlock.classList.add('product__wrapp-active');
+//     });
+
+//  // if(!targetAction) return;
+//  if(currentItem) {
+//     productWrapp.innerHTML = '';
+// }
+// currentItem = targetAction;
+
+
+// catalog.addEventListener('click', (e) => {
+//     const target = e.target;
+//     if(!target.classList.contains('product__wrapp')) return;
+//     if(currentItem) {
+//         currentItem.style.display = 'flex';
+//     }
+//     currentItem = target;
+//     currentItem.style.display = 'none';
+// });
+
+ // mainList.addEventListener('click', (e) => {
+    //     const wrapperSelect = document.querySelectorAll('.product__wrapp');
+    //     wrapperSelect.forEach(el => {
+    //         el.classList.remove('product__wrapp-active');
+    //     });
+    //     wrapperBlock.classList.add('product__wrapp-active');
+    // });
