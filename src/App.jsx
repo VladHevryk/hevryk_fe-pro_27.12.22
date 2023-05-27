4a14de4d492c475218f4f34bbccbe1584dd12a8e
Admin <admin@VladMacBookPro.local>
2023-05-27 23:14:33 +0300

    add home work 26

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
 
 
-function App() {
+const App = () => {
+	return (
+		<div className='container bg-white p-4 mt-5'>
+			<h1>My Todo List</h1>
+			<AddTodoForm />
+			<TodoList />
+		</div>
+	);
+};
 
-  return (
-    <>
-    </>
-  )
-}
-
-export default App
+export default App;
